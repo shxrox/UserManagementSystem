14e5d0553f7ab1a593315c2b0d82eb8439687b97
shxrox <sharondeva8@gmail.com>
2025-04-07 13:47:35 +0530

    JWTUtils (27:32)

---
+++ src/main/java/com/example/usermanagementsystem/service/JWTUtils.java
@@ -0,0 +1,23 @@
+package com.example.usermanagementsystem.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+public class JWTUtils {
+    private SecretKey Key;
+    private static final Long EXPIRATION_TIME = 86400000L; //24hr
+
+    public JWTUtils(){
+        String secreteString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+        byte[] keyBytes = Base64.getDecoder().decode(secreteString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+
+
+}
