64ecf936bf1f9f58c6be1472ed2c84c526a213dc
shxrox <sharondeva8@gmail.com>
2025-04-24 14:37:33 +0530

    OurUserDetailsService updated

--- src/main/java/com/example/usermanagementsystem/repository/UsersRepo.java
+++ src/main/java/com/example/usermanagementsystem/repository/UsersRepo.java
@@ -1,10 +1,12 @@
 package com.example.usermanagementsystem.repository;
 
+
 import com.example.usermanagementsystem.entity.OurUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
-    OurUsers findByUsername(String email);
+
+    Optional<OurUsers> findByEmail(String email);
 }
